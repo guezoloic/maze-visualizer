055fa40c5793b68377b8def0d8e5c3684a9293fd
Spaghetoz <spaghetoz.tgd5@gmail.com>
2025-04-16 11:33:11 +0200

    improve RandomizedDfs
    - single passage dig at once instead of all neighors
    - refactor: add private methods to improve readability and reduce repetition

--- js/algorithms/maze/RandomizedDfs.js
+++ js/algorithms/maze/RandomizedDfs.js
@@ -11,11 +11,37 @@ class RandomizedDfs extends MazeGenAlgorithm {
         this.visited = new Set()
         this.visited.add(this.grid.pos_to_str(start[1], start[0]))
         
+        /** Stores the previous neighbor of each cell added to the stack
+         *  enabling digging a passage 
+         */
+        this.prevs = {}
+        for(let i = 0 ; i < grid.cols; i++) {
+            for(let j = 0; j < grid.rows; j++) {
+                this.prevs[this.grid.pos_to_str(i, j)] = undefined
+            }
+        }
     }
 
     nextStep() {
         if(this.hasNextStep()) {
             let curr = this.stack.pop()
+            
+            let str_curr = this.grid.pos_to_str(curr[0], curr[1])
+            this.visited.add(str_curr)
+
+            this.digIfPossible(curr[1], curr[0])
+
+            let previous = this.prevs[str_curr]
+
+            // Dig a passage between current cell and the cell preceding it
+            if(previous !== undefined) {
+
+                this.digIfPossible(previous[1], previous[0])
+
+                let intermediate_cell = [(curr[0]+previous[0])/2, (curr[1]+previous[1])/2]
+
+                this.digIfPossible(intermediate_cell[1], intermediate_cell[0])
+            }
 
             let neighbors = shuffleArray( this.grid.get_neighbors(curr[0], curr[1], 2))
 
@@ -25,34 +51,34 @@ class RandomizedDfs extends MazeGenAlgorithm {
 
                 if(!this.visited.has(str_neighbor)) {
 
-                    if(this.grid.get(neighbor[1], neighbor[0]) == CellState.WALL) {
-                        this.grid.set(neighbor[1], neighbor[0], CellState.EMPTY)
-                    }
-                    let intermediate_cell = [(curr[0]+neighbor[0])/2, (curr[1]+neighbor[1])/2]
-
-                    if(this.grid.get(intermediate_cell[1], intermediate_cell[0]) == CellState.WALL) {
-                        this.grid.set(intermediate_cell[1], intermediate_cell[0], CellState.EMPTY)
-                    }
-
                     this.stack.push(neighbor)
-                    this.visited.add(str_neighbor)
+                    this.prevs[str_neighbor] = curr
                 }
             }
-            // TODO colorize
-            /*if(this.grid.get(curr[1], curr[0]) != CellState.WALL) {
-                this.grid.set(curr[1], curr[0], CellState.VISITED)
-            }*/
         }
-        //If the algorithm hasn't been marked as over, this is the last step so we mark it as finished
-        else if(!this.isOver()) {
+        // if there isn't next step, finish the algorithm execution 
+        else {
             this.finish()
         }
     }
-
     hasNextStep() {
         return this.stack.length > 0
     }
+
+    markAsVisited(i, j) {
+        if(this.grid.get(i, j) == CellState.EMPTY) {
+            this.grid.set(i, j, CellState.VISITED)
+        }
+    }
+
+    digIfPossible(i, j) {
+        if(this.grid.get(i, j) == CellState.WALL) {
+            this.grid.set(i, j, CellState.EMPTY)
+        } 
+    }
+
     finish() {
         super.finish()
+        this.grid.remove_visited_cells()
     }
 }
