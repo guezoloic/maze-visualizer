deaab667c513cb78a4c438c7e958c92018e8965a
Spaghetoz <spaghetoz.tgd5@gmail.com>
2025-05-23 21:50:06 +0200

    improve CellUndoRedoManager
    - global refactor
    - add redo

--- js/CellUndoRedoManager.js
+++ js/CellUndoRedoManager.js
@@ -1,4 +1,18 @@
 
+/** Defines each action on cells with its corresponding cellstate and opposite action 
+ *  For instance, an "add" action corresponds to adding a WALL cell on the grid
+*/
+const Actions = Object.freeze({    
+    add: {
+        cell_state: CellState.WALL,
+        opposite: "remove",
+    },
+    remove: {
+        cell_state: CellState.EMPTY,
+        opposite: "add",
+    }
+})
+
 /** Handles the cell placement history,
  *  and provide ability to undo / redo placements
  */
@@ -7,7 +21,7 @@ class CellUndoRedoManager {
     constructor(grid) {
         this.grid = grid
         /** The stack of the last cell actions (placement or removal)
-         *  Contains 
+         *  Records are stored in the form : {}
          */
         this.undo_stack = []
         this.redo_stack = []
@@ -16,49 +30,65 @@ class CellUndoRedoManager {
         this.stack_limit = 200
     }
 
-    /** Push to the undo stack a cell placement action*/
+    /** Register a cell placement at a certain position
+     */
     recordCellPlacement(pos) {
-        this._recordCell("add", pos)
+        this._record("add", pos, this.undo_stack)
+
+        this._clearRedoStack()
     }
 
     recordCellRemoval(pos) {
-        this._recordCell("remove", pos)
+        this._record("remove", pos, this.undo_stack)
+
+        this._clearRedoStack()
     }
 
-    _recordCell(action, pos) {
-        this.undo_stack.push({"action": action, "position": pos}) 
-
-        if (this.undo_stack.length > this.stack_limit) {
-            this.undo_stack.shift(); 
-        }
-    }
-
+    /** Revert the last recorded action 
+     *  For example if a wall was deleted at position (9, 1), bring it back 
+    */
     undo() {
-        if(this._undoStackEmpty()) return
+        if(this.undo_stack.length === 0) return
 
-        let last_action = this.undo_stack.pop()
-        let pos = last_action["position"]
-        
-        if(last_action["action"] === "add") {
-            this.grid.set(pos[0], pos[1], CellState.EMPTY)
-        }
-        else if(last_action["action"] === "remove") {
-            this.grid.set(pos[0], pos[1], CellState.WALL)
-        }
+        let { action, position } = this.undo_stack.pop()
+        let oppositeAction = Actions[action]["opposite"]
 
+        this._record(oppositeAction, position, this.redo_stack)
+        this._doActionOnGrid(oppositeAction, position)
     }
 
     redo() {
+        if(this.redo_stack.length === 0) return
+        
+        let { action, position }= this.redo_stack.pop()
+        let oppositeAction = Actions[action]["opposite"]
 
+        this._record(oppositeAction, position, this.undo_stack)
+        this._doActionOnGrid(oppositeAction, position)
     }
 
     resetHistory() {
         this.undo_stack = []
         this.redo_stack = []
     }
+
+    /** Record an action at a certain position to a specific stack 
+     * (for instance undo_stack or redo_stack) */
+    _record(action, pos, toStack) {
+        if (!Actions[action]) throw new Error(`Invalid action: ${action}`)
+
+        if (toStack.length > this.stack_limit) {
+            toStack.shift(); 
+        }
+
+        toStack.push({"action": action, "position": pos}) 
+    }
     
-    _undoStackEmpty() {
-        return this.undo_stack.length == 0
+    _clearRedoStack() {
+        this.redo_stack = []
     }
 
+    _doActionOnGrid(action, pos) {
+        this.grid.set(pos[0], pos[1], Actions[action]["cell_state"])
+    }
 }
